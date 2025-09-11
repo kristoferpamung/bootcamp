078ce5d5b14d7a84ae7a8e302043fa4bc34539e4
Kristofer Bayu <kristoferpamung@gmail.com>
2025-09-11 11:54:00 +0700

    Add Library Class in mini project

--- Practices/Day3-Creating-Types/Program.cs
+++ Practices/Day3-Creating-Types/Program.cs
@@ -111,3 +111,77 @@ public class Book
     }
 }
 
+class Library
+{
+    // readonly hanya dapat di set di constructor
+    public static readonly string LibraryName = "City Central Library";
+    private List<Book> _books;
+
+    static Library()
+    {
+        Console.WriteLine("Initializing Library System...");
+    }
+
+    public Library()
+    {
+        _books = new List<Book>();
+    }
+
+    public Book this[int index]
+    {
+        get
+        {
+            if (index > _books.Count() - 1)
+            {
+                return null!;
+            }
+            else
+            {
+                return _books[index];
+            }
+        }
+        set
+        {
+            if (index > _books.Count() - 1)
+            {
+                Console.WriteLine("Argument out of range!");
+            }
+            _books[index] = value;
+        }
+    }
+
+    public Book this[string title]
+    {
+        get
+        {
+            Book bookFound = _books.Find(book => book.Title == title);
+            if (bookFound != null)
+            {
+                return bookFound;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        set
+        {
+
+        }
+    }
+
+    public void AddBook(Book book)
+    {
+        _books.Add(book);
+    }
+
+    public int GetTotalBook()
+    {
+        return _books.Count();
+    }
+
+    public void PrintAllBooks()
+    {
+        _books.ForEach((book) => book.DisplayBookDetails());
+    }
+}
