c3bc63a0506a50fbcb5601aabb1982b46adedb30
Kristofer Bayu <kristoferpamung@gmail.com>
2025-09-11 14:52:40 +0700

    Add example Inheritance

--- Practices/Basic/Classes/Program.cs
+++ Practices/Basic/Classes/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Stock s = new();
+		s.Name = "MSFT";
+		s.SharedOwned = 1000L;
+		
+		House h = new();
+		h.Name = "Mansion";
+		s.SharedOwned = 250_000L;
+
+        Console.WriteLine(h.GetType());
+		
+		Console.WriteLine(s.Name + " " + s.SharedOwned);
+		Console.WriteLine(h.Name + " " + h.Mortgage);
+	}
+}
+
+public class Assets {
+	public string Name = "";
+}
+
+public class Stock : Assets
+{
+    public long SharedOwned;
+}
+
+public class House : Assets {
+	public decimal Mortgage;
+}
