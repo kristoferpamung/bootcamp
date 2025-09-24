633b51d0aa220681d7368c17626a2ab28bb8545c
Kristofer Bayu <111991943+kristoferpamung@users.noreply.github.com>
2025-09-24 07:10:47 +0700

    Update Logic Program.cs

--- LogicExercise/Program.cs
+++ LogicExercise/Program.cs
@@ -1,14 +1,61 @@
-﻿
+using System.Text;
+
 string? numberString;
 
 Console.Write("input n number: ");
 numberString = Console.ReadLine();
 
-if (numberString == null || numberString == "")
+if (string.IsNullOrEmpty(numberString))
 {
+    Console.WriteLine("Input can't empty or null");
     return;
 }
 
+StringBuilder outputString = new();
+
+if (int.TryParse(numberString, out int n))
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0)
+        {
+            outputString.Append("foo");
+        }
+        if (i % 4 == 0)
+        {
+            outputString.Append("baz");
+        }
+        if (i % 5 == 0)
+        {
+            outputString.Append("bar");
+        }
+        if (i % 7 == 0)
+        {
+            outputString.Append("jazz");
+        }
+        if (i % 9 == 0)
+        {
+            outputString.Append("huzz");
+        }
+        if (i % 3 != 0 && i % 4 != 0 && i % 5 != 0 && i % 7 != 0 && i % 9 != 0)
+        {
+            outputString.Append(i);
+        }
+        if (i < n)
+        {
+            outputString.Append(", ");
+        }
+        if (i == n)
+        {
+            outputString.Append('.');
+        }
+    }
+}
+
+Console.WriteLine(outputString);
+
+/* MY OLD LOGIC */
+/*
 if (int.TryParse(numberString, out int n))
 {
     for (int i = 1; i <= n; i++)
@@ -63,3 +110,4 @@ if (int.TryParse(numberString, out int n))
         }   
     }
 }
+*/
